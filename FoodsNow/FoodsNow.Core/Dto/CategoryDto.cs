a57868c55126c7e5c94afcb813ee8717eec47d61
Muhammad Atif <atif.gk73@yahoo.com>
2023-08-01 12:47:00 +0500

    changes

--- FoodsNow/FoodsNow.Core/Dto/CategoryDto.cs
+++ FoodsNow/FoodsNow.Core/Dto/CategoryDto.cs
@@ -4,7 +4,6 @@
     {
         public required Guid Id { get; set; }
         public required string Name { get; set; }
-        public required string AppLogo { get; set; }
         public required string Cover { get; set; }
         public required string Thumbnail { get; set; }
         public string? Logo { get; set; }
