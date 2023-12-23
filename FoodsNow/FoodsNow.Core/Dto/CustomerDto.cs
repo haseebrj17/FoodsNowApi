4d7d7a5d588d5e50cb3dcaa300e22589646ecd38
Habib Bhatti <habibbhatti90@gmail.com>
2023-12-23 12:13:39 +0500

    VerificationCode and Device Token update

--- FoodsNow/FoodsNow.Core/Dto/CustomerDto.cs
+++ FoodsNow/FoodsNow.Core/Dto/CustomerDto.cs
@@ -7,8 +7,9 @@ namespace FoodsNow.Core.Dto
         public Guid? Id { get; set; }
         public required string FullName { get; set; }
         public required string EmailAdress { get; set; }
+        public string? Password { get; set; }
         public required string ContactNumber { get; set; }
-        public string? Code { get; set; }
+        public string? VerificationCode { get; set; }
         public UserRole? UserRole { get; set; }
         public required string DeviceToken { get; set; }
     }
